03c4e52a5e5e757127c35a626c31efd5abdeb3e1
Jay Kim <jaykimcodes@gmail.com>
2024-04-13 17:09:58 -0400

    update

--- init.lua
+++ init.lua
@@ -78,7 +78,7 @@ vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- enable fzf-lua
-vim.keymap.set('n', '<c-P>', "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+vim.keymap.set('n', '<leader>p', "<cmd>lua require('fzf-lua').files()<CR>", { silent = true, desc = 'Fuzzy find' })
 
 vim.keymap.set('i', 'jk', '<ESC>', { desc = 'Exit insert mode with jk' })
 
@@ -210,19 +210,16 @@ require('lazy').setup({
 
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
+    event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
     end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
   },
 
   -- NOTE: Plugins can specify dependencies.
@@ -936,6 +933,9 @@ require('lazy').setup({
       vim.keymap.set('x', 's', substitute.visual, { desc = 'Substitute in visual mode' })
     end,
   },
+
+  -- mark files to quickly switch back and forth
+  --
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
