7076d19fd4b91ef8e402eb662a2f8aa3bee16040
Jay <jaykimcodes@gmail.com>
2024-04-15 14:08:23 -0400

    update

--- init.lua
+++ init.lua
@@ -99,13 +99,6 @@ vim.keymap.set('v', '<Esc>', '<Esc>`y', { noremap = true, silent = true })
 
 vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
 
--- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
--- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
--- is not what someone will guess without a bit more experience.
--- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
--- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
 -- Keybinds to make split navigation easier.
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
