14a27cdf8705d603e1a75073576af2d4b69c0722
Jay <jaykimcodes@gmail.com>
2024-04-15 12:55:55 -0400

    update

--- init.lua
+++ init.lua
@@ -337,7 +337,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        tsserver = {},
+        eslint = {},
         ember = {},
+
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
