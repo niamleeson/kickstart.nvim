7008ce2d7c81d6c024d211236d1dd0d27b82f2f9
Jay <jaykimcodes@gmail.com>
2024-04-15 20:54:12 -0400

    update

--- init.lua
+++ init.lua
@@ -207,7 +207,7 @@ require('lazy').setup({
         c = {
           name = 'Code',
         },
-        l = {
+        f = {
           name = 'Fuzzy find',
           v = {
             name = 'Git',
@@ -947,6 +947,26 @@ require('lazy').setup({
     config = function()
       require('colorizer').setup {}
     end,
+  },
+
+  {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
   },
 }, {
   ui = {
