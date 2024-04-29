b41b318d4909ff09911fbab0769ec08252c7ec1b
Jay Kim <jaykimcodes@gmail.com>
2024-04-29 17:15:42 -0400

    update

--- init.lua
+++ init.lua
@@ -825,11 +825,11 @@ require('lazy').setup({
         },
       }
 
-      -- local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
-      --
-      -- -- vim way: ; goes to the direction you were moving.
-      -- vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
-      -- vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
+      local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+
+      -- vim way: ; goes to the direction you were moving.
+      vim.keymap.set({ 'n', 'x', 'o' }, ';', ts_repeat_move.repeat_last_move)
+      vim.keymap.set({ 'n', 'x', 'o' }, ',', ts_repeat_move.repeat_last_move_opposite)
       --
       -- -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
       -- vim.keymap.set({ 'n', 'x', 'o' }, 'f', ts_repeat_move.builtin_f)
