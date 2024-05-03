6d7029d7495ebbe4be13a8969336ecf1c99f7c66
Jay Kim <jaykimcodes@gmail.com>
2024-05-02 22:11:42 -0400

    update

--- init.lua
+++ init.lua
@@ -1410,15 +1410,6 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   'hedyhli/outline.nvim',
-  --   config = function()
-  --     vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
-  --
-  --     require('outline').setup {}
-  --   end,
-  -- },
-
   {
     'stevearc/aerial.nvim',
     opts = {},
