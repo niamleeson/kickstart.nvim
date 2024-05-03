c1a2cf4f6c99c71dd9ec26c2ef5756a04c86a240
Jay Kim <jaykimcodes@gmail.com>
2024-05-03 16:02:38 -0400

    add mouse scroll speed

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
+vim.opt.mousescroll = 'ver:10'
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
