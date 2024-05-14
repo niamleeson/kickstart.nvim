9c0e9aaea7a1a725f3f20262ad5a1f4b3f91af80
Jay Kim <jaykimcodes@gmail.com>
2024-05-14 01:20:04 -0400

    update

--- init.lua
+++ init.lua
@@ -2170,6 +2170,6 @@ return config
 
 
 -- key repeat in macos
-defaults write -g InitialKeyRepeat -int 10 # normal minimum is 15 (225 ms)
+defaults write -g InitialKeyRepeat -int 13 # normal minimum is 15 (225 ms)
 defaults write -g KeyRepeat -int 1 # normal minimum is 2 (30 ms)
 --]]
