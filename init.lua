b34a3a2a2bf587ad4579804ece505b52fc70c620
Jay Kim <jaykimcodes@gmail.com>
2025-03-15 14:00:28 -0400

    add vim indent object

--- init.lua
+++ init.lua
@@ -2074,6 +2074,29 @@ require('lazy').setup({
 		-- vim.api.nvim_set_keymap('n', '<leader>gr', '"0gr', { noremap = false, silent = true })
 	-- end,
   },
+
+  {
+	"jessekelighine/vindent.nvim",
+	config = function()
+		local map = {
+			motion = function(lhs, rhs) vim.keymap.set("", lhs, rhs) end,
+			object = function(lhs, rhs) vim.keymap.set({ "x", "o" }, lhs, rhs) end,
+		}
+		map.motion("[=", "<Plug>(VindentBlockMotion_OO_prev)")
+		map.motion("]=", "<Plug>(VindentBlockMotion_OO_next)")
+		map.motion("[+", "<Plug>(VindentMotion_more_prev)")
+		map.motion("]+", "<Plug>(VindentMotion_more_next)")
+		map.motion("[-", "<Plug>(VindentMotion_less_prev)")
+		map.motion("]-", "<Plug>(VindentMotion_less_next)")
+		map.motion("[;", "<Plug>(VindentMotion_diff_prev)")
+		map.motion("];", "<Plug>(VindentMotion_diff_next)")
+		map.motion("[p", "<Plug>(VindentBlockEdgeMotion_XX_prev)")
+		map.motion("]p", "<Plug>(VindentBlockEdgeMotion_XX_next)")
+		map.object("ii", "<Plug>(VindentObject_XX_ii)")
+		map.object("ai", "<Plug>(VindentObject_XX_ai)")
+		map.object("aI", "<Plug>(VindentObject_XX_aI)")
+	end
+  },
   -- zzz
 }, {
   ui = {
